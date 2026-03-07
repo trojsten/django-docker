fafc92505de977e86efb4b156084aa7fc482004d
Adam Zahradník <adam@zahradnik.xyz>
2026-03-07 22:29:53 +0100

    feat: allow passing gunicorn flags

--- base/gunicorn.sh
+++ base/gunicorn.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-exec gunicorn --bind 127.0.0.1:8001 --access-logfile - --max-requests 2000 --max-requests-jitter 100 $1
+exec gunicorn --bind 127.0.0.1:8001 --access-logfile - --max-requests 2000 --max-requests-jitter 100 "$@"
